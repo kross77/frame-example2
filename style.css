d4524988d383f5cb237e014e96d3aebfacd68829
Ales Krasouski <a.krasouski@invatechs.com>
2017-12-17 23:14:11 +0300

    Initial commit

---
+++ style.css
@@ -0,0 +1,108 @@
+@import url('https://fonts.googleapis.com/css?family=Alegreya:400,700');
+
+
+body{
+  font-family: 'Alegreya'
+}
+
+h2{
+  font-weight: bold;
+  font-size: 4em;
+  text-align: center;
+}
+
+.source{
+  font-size: 2.5em;
+  text-align: center;
+}
+
+.ramka{
+  position: relative;
+  margin: 0 auto;
+  width: 90vw;
+  max-width: 800px;
+  min-width: 400px;
+  min-height: 260px;
+}
+
+.u1, .u2, .u3, .u4{
+  background-image: url('http://fe.it-academy.by/Sites/0024789/ugolok.png');
+  width: 130px;
+  height: 130px;
+  background-size: contain;
+}
+
+.u1, .u2, .u3, .u4{
+  position: absolute;
+}
+
+.u2{
+  right: 0;  
+  transform: rotate(90deg)
+}
+
+.u3{
+  bottom: 0;
+  transform: rotate(-90deg)
+}
+
+.u4{
+  right: 0;
+  bottom: 0;
+  transform: rotate(180deg)
+}
+
+
+.border-top, .border-bottom{
+  position: absolute;
+  height: 15px;
+  background-size: contain;
+  left: 130px;
+  right: 130px;
+  background-image: url('http://fe.it-academy.by/Sites/0024789/side.png');
+}
+
+.border-left, .border-right{
+  position: absolute;
+  top: 130px;
+  bottom: 130px;
+  width: 15px;
+  background-size: contain;
+  background-image: url('http://fe.it-academy.by/Sites/0024789/side2.png');
+}
+
+.border-bottom{
+  bottom: 2px;
+}
+
+.border-right{
+  right: 1px;
+}
+
+.content{
+  position: relative;
+  font-size: 2em;
+  padding: 50px;
+  text-align: center;
+}
+
+.button-block{
+  height: 100px;
+  width: 100%;
+  position: relative;
+}
+
+#nextQuote{
+  position: absolute;
+  font-size: 1.5em;
+  text-align: center;
+  width: 360px;
+  color: black;
+  font-weight: 700;
+  border: 15px double black;
+  text-decoration: none;
+  padding: 10px 0;
+  margin-left: -180px;
+  left: 50%;
+  text-transform: uppercase;
+}
